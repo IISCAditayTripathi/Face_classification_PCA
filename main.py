9fa335aa676024aac1719fdcf673d80ab0b57aad
IISCAditayTripathi <aditayt@iisc.ac.in>
2018-08-25 21:10:07 -0400

    Implementing K-NN

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import numpy
 import pickle
 from my_pca import PrincleComponentAnalysis as pca
 from skimage import io as IO
+from sklearn.neighbors import KNeighborsClassifier
 
 file_path= '/home/aditay/Matrix_Theory/Assignments/Assignment_2_PCA/code/face_image_paths.txt'
 dataIO = read_and_load(file_path)
 
 train_data, train_mean, train_class, valid_data, valid_mean, valid_class = dataIO.make_train_test()
+
+
 bias = train_mean - valid_mean
+
 
 principle_components = pca(train_data)
 
@@ -19,21 +23,33 @@ principle_components = pca(train_data)
 U, S, V = principle_components.load_pc()
 
 
-k = 10 # Number of PCA dimensions
+k = 1000 # Number of PCA dimensions
 
+reduced_U = numpy.transpose(U[:, 0:k])
+reduced_images_train = numpy.matmul(reduced_U, numpy.transpose(train_data))
 
+reduced_images_valid = numpy.matmul(reduced_U, numpy.transpose(valid_data))
+
+neigh = KNeighborsClassifier(n_neighbors=8)
+
+neigh.fit(numpy.transpose(reduced_images_train), numpy.asarray(train_class))
+predictions = neigh.predict(numpy.transpose(reduced_images_valid))
+print(predictions)
+'''
 train_sample = dataIO.sample_image(train_data)
-test_sample = valid_data[0,:]
+test_sample = valid_data[1,:]
 
 reduced_image, reduced_U = principle_components.reduce_image(U, test_sample, k)
 
-reconstruced_image = principle_components.reconstruct_image(reduced_image, reduced_U, valid_mean)
+reconstruced_image = principle_components.reconstruct_image(reduced_image, reduced_U, valid_mean) + 90
 
 original_image = test_sample + valid_mean - bias
 test_mean_face = numpy.asarray(valid_mean.reshape([112, 92]), dtype=numpy.int16)
 
 original_reshaped = original_image.reshape([112, 92])
 original_reshaped = numpy.asarray(original_reshaped, dtype=numpy.int16)
-IO.imsave('test_original_reshaped_8.jpg', original_reshaped)
-IO.imsave('test_reconstruced_image_8_k_10.jpg', reconstruced_image)
+
+IO.imsave('test_original_reshaped_1.jpg', original_reshaped)
+IO.imsave('test_reconstruced_image_1_k_30.jpg', reconstruced_image)
 IO.imsave('test_mean_face.jpg', test_mean_face)
+'''
--- read_data.py
+++ read_data.py
@@ -21,22 +21,34 @@ class read_and_load(object):
 
     def read_data(self, data_list):
 
-        image_array = []
-        image_class = []
+        image_array_train = []
+        image_array_valid = []
+        image_class_train = []
+        image_class_valid = []
         z = 0
         for data in data_list:
             z = z + 1
-            for i in range(10):
+            for i in range(8):
                 data_path = data[0:-1]+'/'+str(i+1)+'.pgm'
                 im = IO.imread(data_path)
                 im = [elements for rows in im for elements in rows ]
-                image_array.append(im)
-                image_class.append(z)
-        image_array = numpy.asarray(image_array)
-        mean_face = numpy.mean(image_array, axis=0)
-        image_array = (image_array - mean_face)
+                image_array_train.append(im)
+                image_class_train.append(z)
+            for i in range(2):
+                data_path = data[0:-1]+'/'+str(i+9)+'.pgm'
+                im = IO.imread(data_path)
+                im = [elements for rows in im for elements in rows ]
+                image_array_valid.append(im)
+                image_class_valid.append(z)
+        image_array_train = numpy.asarray(image_array_train)
+        mean_face_train = numpy.mean(image_array_train, axis=0)
+        image_array_train = (image_array_train - mean_face_train)
 
-        return image_array, mean_face, image_class
+        image_array_valid = numpy.asarray(image_array_valid)
+        mean_face_valid = numpy.mean(image_array_valid, axis=0)
+        image_array_valid = (image_array_train - mean_face_valid)
+
+        return image_array_train, mean_face_train, image_class_train, image_array_valid, mean_face_valid, image_class_valid
 
 
     def make_train_test(self):
@@ -45,11 +57,11 @@ class read_and_load(object):
         data_list = data_list.readlines()
         n_data = len(data_list)
         data_split = int(0.8 * n_data)
-        train_data, train_mean, train_class = self.read_data(data_list[0:data_split])
+        train_data, train_mean, train_class, valid_data, valid_mean, valid_class = self.read_data(data_list)
         print("training set size:(%d,%d)"%(train_data.shape[0], train_data.shape[1]))
-        validation_data, valid_mean, valid_class = self.read_data(data_list[data_split:])
-        print("validation set size:(%d,%d)"%(validation_data.shape[0], validation_data.shape[1]))
-        return (train_data, train_mean, train_class, validation_data, valid_mean, valid_class)
+        # validation_data, valid_mean, valid_class = self.read_data(data_list[data_split:])
+        print("validation set size:(%d,%d)"%(valid_data.shape[0], valid_data.shape[1]))
+        return (train_data, train_mean, train_class, valid_data, valid_mean, valid_class)
 
     def sample_image(self, image_array):
         max = image_array.shape[0]
